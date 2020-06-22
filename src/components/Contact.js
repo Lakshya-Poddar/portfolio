af140174b4b9415c1568a6c4f2308efb656a57a2
Lakshya-Poddar <lakshyapoddar1604@gmail.com>
2020-06-22 21:11:09 +0530

    V1.2.0

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Contact() {
     return (
         <div>
-            Page Under Development!
+            <h1 style={{textAlign:"center"}}>Page Under Development!</h1>
         </div>
     )
 }

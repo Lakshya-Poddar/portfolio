a927bfed89490b5012932d5948d86997afc6c08e
Lakshya-Poddar <lakshyapoddar1604@gmail.com>
2020-07-05 14:22:04 +0530

    about

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -12,7 +12,7 @@ function NavHeader() {
                             <Navbar.Collapse id="responsive-navbar-nav">
                                     <Nav className="ml-auto">
                                     <Nav.Link href="#"><Link to='/' className={darkmode?"linkdark":"linklight"}>HOME</Link></Nav.Link>
-                                    {/* <Nav.Link href="#"><Link to='/about' className={darkmode?"linkdark":"linklight"}>ABOUT</Link></Nav.Link> */}
+                                    <Nav.Link href="#"><Link to='/about' className={darkmode?"linkdark":"linklight"}>ABOUT</Link></Nav.Link>
                                     <Nav.Link href="#"><Link to='/projects' className={darkmode?"linkdark":"linklight"}>PROJECTS</Link></Nav.Link>
                                     <Nav.Link href="#"><Link to='/contact' className={darkmode?"linkdark":"linklight"}>CONTACT</Link></Nav.Link>
                                     <Nav.Link href="https://drive.google.com/u/0/uc?id=1hX7U5ULQadijDGT0rYFX7GLUrHweBiMf&export=download" className={darkmode?"linkdark":"linklight"}>RESUME</Nav.Link>
